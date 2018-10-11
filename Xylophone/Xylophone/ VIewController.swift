b802fd4f609ece90904c5123654b45fa3ce6cf2d
SuperMacUser <eriksson.hans@gmail.com>
2018-10-11 20:29:26 +0200

    Fun small turtotial

---
+++ Xylophone/Xylophone/ VIewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+    
+    var audioPlayer : AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        playTone(tagID: sender.tag)
+    }
+    
+    func playTone(tagID : Int) {
+        let soundPlay = Bundle.main.url(forResource: "note\(tagID)", withExtension: "wav")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundPlay!)
+        } catch {
+            print (error)
+        }
+        audioPlayer.play()
+       
+    }
+}
+    
+  
+
+
+
